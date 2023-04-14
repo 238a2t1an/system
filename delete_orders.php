6bd5566b7006451f7dfcf761f4909e8c80f536b3
238a2t1an <130731319+238a2t1an@users.noreply.github.com>
2023-04-14 14:15:33 +0800

    Add files via upload

--- delete_orders.php
+++ delete_orders.php
@@ -1,9 +1,11 @@
 <?php
-include("../connection/connect.php");
+include("connection/connect.php"); //connection to db
 error_reporting(0);
 session_start();
 
-mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'");
-header("location:all_orders.php");  
+
+// sending query
+mysqli_query($db,"DELETE FROM users_orders WHERE o_id = '".$_GET['order_del']."'"); 
+header("location:your_orders.php"); 
 
 ?>
